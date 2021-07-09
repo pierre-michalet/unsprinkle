4c9f173e1c41d422a714e97e4ca79d7f31f95dfd
Pierre Michalet <pierre.michalet@blablacar.com>
2021-07-09 08:49:40 +0200

    workshop done ! (minus the ellipsis :( )

--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
 const PhotoGridItem = ({ id, src, alt, tags }) => {
+  const srcJpg1x = src;
+  const srcJpg2x = src.replace(".jpg", "@2x.jpg");
+  const srcJpg3x = src.replace(".jpg", "@3x.jpg");
+
+  const srcAvif1x = src;
+  const srcAvif2x = src.replace(".jpg", "@2x.avif");
+  const srcAvif3x = src.replace(".jpg", "@3x.avif");
+
   return (
     <article>
       <Anchor href={`/photos/${id}`}>
-        <Image src={src} />
+        <picture>
+          <source
+            type='image/avif'
+            srcSet={`${srcAvif1x} 1x, ${srcAvif2x} 2x, ${srcAvif3x} 3x`}
+          ></source>
+          <source
+            type='image/jpg'
+            srcSet={`${srcJpg1x} 1x, ${srcJpg2x} 2x, ${srcJpg3x} 3x`}
+          ></source>
+
+          <Image src={src} alt={tags.join(",")} />
+        </picture>
       </Anchor>
       <Tags>
         {tags.map((tag) => (
@@ -28,15 +47,18 @@ const Image = styled.img`
   height: 300px;
   border-radius: 2px;
   margin-bottom: 8px;
+  object-fit: cover;
 `;
 
 const Tags = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 8px;
+  width: 100%;
+  overflow: hidden;
+  white-space: nowrap;
 `;
 
 const Tag = styled.li`
+  display: inline-block;
+  margin-right: 6px;
   padding: 4px 8px;
   background: var(--color-gray-300);
   font-size: 0.875rem;
